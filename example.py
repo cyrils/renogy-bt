b47103a0084d2a788ba558cbe87fbd2566f15840
Cyril Sebastian <cyrils@users.noreply.github.com>
2022-02-06 13:08:43 +0530

    update

--- example.py
+++ example.py
@@ -6,7 +6,7 @@ logging.basicConfig(level=logging.DEBUG)
 def data_callback(data):
     logging.debug(data)
 
-bt1 = BTOneApp("hci0", "80:6F:B0:0F:EA:2B", "BT-TH-B00FEA2B", data_callback)
+bt1 = BTOneApp("hci0", "80:6F:B0:0F:XX:XX", "BT-TH-B00FXXX", data_callback)
 bt1.connect()
 
 
