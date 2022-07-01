2bf350d651ab17282535e54d87c5337c42dbd36c
Cyril Sebastian <cyrils@users.noreply.github.com>
2022-06-30 23:26:38 -0700

    cleanup

--- example.py
+++ example.py
@@ -3,6 +3,7 @@ from BTOneApp import BTOneApp
 
 logging.basicConfig(level=logging.DEBUG)
 
+ADAPTER = "hci0"
 MAC_ADDR = "80:6F:B0:0F:XX:XX"
 DEVICE_ALIAS = "BT-TH-B00FXXXX"
 POLL_INTERVAL = 30 # read data interval (seconds)
@@ -14,5 +15,5 @@ def on_data_received(app: BTOneApp, data):
     logging.debug("{} => {}".format(app.device.alias(), data))
     # app.disconnect() # disconnect here if you do not want polling
 
-bt1 = BTOneApp("hci0", MAC_ADDR, DEVICE_ALIAS, on_connected, on_data_received, POLL_INTERVAL)
+bt1 = BTOneApp(ADAPTER, MAC_ADDR, DEVICE_ALIAS, on_connected, on_data_received, POLL_INTERVAL)
 bt1.connect()
