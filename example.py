cc13d1e198d6ea20f7dbef33cc229a3da0def148
Cyril Sebastian <cyrils@users.noreply.github.com>
2023-02-01 18:40:24 -0800

    pvoutput

--- example.py
+++ example.py
@@ -9,13 +9,15 @@ config = configparser.ConfigParser()
 config.read('config.ini')
 data_logger: DataLogger = DataLogger(config)
 
-def on_data_received(client, data):
+def on_data_received(client: BTOneClient, data):
     logging.debug("{} => {}".format(client.device.alias(), data))
-    if client.config['remote_logging']['enabled'] == True:
+    if client.config['remote_logging'].getboolean('enabled'):
         data_logger.log_remote(json_data=data)
-    if client.config['mqtt']['enabled'] == True:
+    if client.config['mqtt'].getboolean('enabled'):
         data_logger.log_mqtt(json_data=data)
-    if client.config['device']['poll_data'] != True:
+    if client.config['pvoutput'].getboolean('enabled'):
+        data_logger.log_pvoutput(json_data=data)
+    if not client.config['device'].getboolean('enable_polling'):
         client.disconnect()
 
 logging.info(f"Starting client: {config['device']['alias']} => {config['device']['mac_addr']}")
--- renogybt/DataLogger.py
+++ renogybt/DataLogger.py
@@ -1,8 +1,11 @@
-from configparser import ConfigParser
 import json
 import logging
 import requests
+from configparser import ConfigParser
+from datetime import datetime
 import paho.mqtt.publish as publish
+
+PVOUTPUT_URL = 'http://pvoutput.org/service/r2/addstatus.jsp'
 
 class DataLogger:
     def __init__(self, config: ConfigParser):
@@ -11,7 +14,7 @@ class DataLogger:
     def log_remote(self, json_data):
         headers = { "Authorization" : f"Bearer {self.config['remote_logging']['auth_header']}" }
         req = requests.post(self.config['remote_logging']['url'], json = json_data, timeout=15, headers=headers)
-        return req.status_code == 200
+        print("Log remote 200" if req.status_code == 200 else f"Log remote error {req.status_code}")
 
     def log_mqtt(self, json_data):
         logging.info(f"mqtt logging")
@@ -25,3 +28,13 @@ class DataLogger:
             auth=auth,
             client_id="renogy-bt"
         )
+    
+    def log_pvoutput(self, json_data):
+        date_time = datetime.now().strftime("d=%Y%m%d&t=%H:%M")
+        data = f"{date_time}&v1={json_data['power_generation_today']}&v2={json_data['pv_power']}&v3={json_data['power_consumption_today']}&v4={json_data['load_power']}&v5={json_data['controller_temperature']}&v6={json_data['battery_voltage']}"
+        response = requests.post(PVOUTPUT_URL, data=data, headers={
+            "Content-Type": "application/x-www-form-urlencoded",
+            "X-Pvoutput-Apikey": self.config['pvoutput']['api_key'],
+            "X-Pvoutput-SystemId":  self.config['pvoutput']['system_id']
+        })
+        print(f"pvoutput {response}")
